a05577e052c251f6b52f48f77ec8b9e768e64d53
yoohannes <yohannesgidey420@gmail.com>
2021-08-31 15:40:14 +0200

    added manual word count

---
+++ main.py
@@ -0,0 +1,24 @@
+def count(data):
+    new_dict={}
+
+    data=data.split()
+    for i in data:
+        if i in new_dict.keys():
+            new_dict[i]=new_dict.get(i)+1
+        else:
+            new_dict[i]=0
+
+    print(new_dict)
+
+def map_reduce_job():
+    #TODO write a map reduce job for word counter
+
+
+
+
+if __name__ == '__main__':
+    with open('data.txt') as f:
+        data=f.read()
+    count(data)
+
+
